15c2f3d8e23d73766cc2fe795fd64fb95bea27a7
Francis Rocco <roccofrancis@gmail.com>
2016-10-21 17:34:55 -0400

    render proper article data

--- src/components/articles_index.js
+++ src/components/articles_index.js
@@ -6,6 +6,33 @@ function ArticlesIndex (props) {
   return (
   <div>
     <h1>DA ARTICLES</h1>
+
+      <div className="table-responsive">
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>UNPUBLISHED ARTICLES ({props.articles.length})</th>
+            <th>AUTHOR</th>
+            <th>WORDS</th>
+            <th>SUBMITTED</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+          {props.articles.map((article) =>
+            <tr>
+              <td>{article.title}</td>
+              <td>{article.author.name}</td>
+              <td>{article.word_count}</td>
+              <td>YAS</td>
+            </tr>
+          )}
+
+        </tbody>
+      </table>
+    </div>
+
   </div>
   )
 };
