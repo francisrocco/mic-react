171ad601b4cf091491dc76c66e877354c34bc127
Francis Rocco <roccofrancis@gmail.com>
2016-10-25 14:26:25 -0400

    use article file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,7 @@
 const databaseUrl = 'http://localhost:3000/api/v1/'
 
 export function fetchArticles(){
-  const articles = fetch(`${databaseUrl}articles`).then(response => {
-    return response.json()
-  }).then(articlesPayload => {
-    return articlesPayload
-  })
-
+  const articles = require('../../data/articles.json')
   return {
     type: 'FETCH_ARTICLES',
     payload: articles
--- src/components/articles_index.js
+++ src/components/articles_index.js
@@ -1,44 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import moment from 'moment'
 
-function ArticlesIndex (props) {
-  return (
-  <div>
-    <div className="table-responsive">
-      <table className="table table-striped table-bordered">
-        <thead>
-          <tr className="table-header vert-align-mid-head">
-            <th><br/>UNPUBLISHED ARTICLES ({props.articles.length})<br/><br/></th>
-            <th>AUTHOR<br/><br/></th>
-            <th>WORDS<br/><br/></th>
-            <th>SUBMITTED<br/><br/></th>
-          </tr>
-        </thead>
-        <tbody>
+class ArticlesIndex extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-          {props.articles.map((article) =>
-            <tr>
-              <td className="article-cell">
-                <div>
-                  <img className="article-avatar" src={article.avatar}/>
-                  <div className="article-title"> {article.title} </div>
-                </div>
-                { article.bootcamp ? <div className="bootcamp">Bootcamp</div> : null }
-              </td>
-              <td className="author-name vert-align-mid">{article.author.name}</td>
-              <td className="text-center vert-align-mid">{article.word_count}</td>
-              <td className="text-center vert-align-mid">{article.submitted}</td>
-            </tr>
-          )}
+  render() {
+    return (
+      <div>
+        <div className="table-responsive">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr className="table-header vert-align-mid-head">
+                <th><br/>UNPUBLISHED ARTICLES ({this.props.articles.length})<br/><br/></th>
+                <th>AUTHOR<br/><br/></th>
+                <th>WORDS<br/><br/></th>
+                <th>SUBMITTED<br/><br/></th>
+              </tr>
+            </thead>
+            <tbody>
 
-        </tbody>
-      </table>
-    </div>
+              {this.props.articles.map((article) =>
+                <tr>
+                  <td className="article-cell">
+                    <div>
+                      <img className="article-avatar" src={article.image}/>
+                      <a href= {article.url} target="_blank"> {article.title} </a>
+                    </div>
+                    { article.bootcamp ? <div className="bootcamp">Bootcamp</div> : null }
+                  </td>
+                  <td className="author-name vert-align-mid">{article.profile.first_name} {article.profile.last_name}</td>
+                  <td className="text-center vert-align-mid">{article.words}</td>
+                  <td className="text-center vert-align-mid">{ moment(article.publish_at).fromNow() }</td>
+                </tr>
+              )}
 
-    <Link to="/articles/new" className="btn btn-default btn-block">Add An Article</Link><br/>
-  </div>
-  )
+            </tbody>
+          </table>
+        </div>
+
+        <Link to="/articles/new" className="btn btn-default btn-block">Add An Article</Link><br/>
+      </div>
+    )
+  }
 };
 
 function mapStateToProps(state){
