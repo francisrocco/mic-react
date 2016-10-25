de77b8c3c542f7841a0111920d70ad0ceee16ebb
Francis Rocco <roccofrancis@gmail.com>
2016-10-25 19:56:56 -0400

    add sorting feature for word count and submitted

--- src/components/articles_index.js
+++ src/components/articles_index.js
@@ -2,28 +2,53 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import moment from 'moment'
+import {Icon} from 'react-fa'
+
 
 class ArticlesIndex extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { limit: 10, showMore: true }
+    this.state = { limit: 10, showMore: true, sort: true }
     this.showMore = this.showMore.bind(this)
     this.renderButton = this.renderButton.bind(this)
+    this.sortWordsLow = this.sortWordsLow.bind(this)
+    this.sortSubmittedLow = this.sortSubmittedLow.bind(this)
+    this.sortWordsHigh = this.sortWordsHigh.bind(this)
+    this.sortSubmittedHigh = this.sortSubmittedHigh.bind(this)
   }
 
   showMore() {
-    debugger
     this.setState({
       limit: this.state.limit + 10,
-      showMore: this.state.limit + 10 < this.props.articles.length
+      showMore: (this.state.limit < this.props.articles.length)
     });
   }
 
   renderButton() {
     if (!this.state.showMore) return null;
     return (
-      <button className="btn btn-default btn-block" onClick={this.showMore}>Show More</button>
+      <button className="btn btn-default btn-block" onClick={this.showMore}>Load More</button>
     );
+  };
+
+  sortWordsLow() {
+    this.props.articles.sort(sortBy('words', false, parseInt))
+    this.forceUpdate()
+  }
+
+  sortSubmittedLow() {
+    this.props.articles.sort(sortBy('publish_at', false))
+    this.forceUpdate()
+  }
+
+  sortWordsHigh() {
+    this.props.articles.sort(sortBy('words', true, parseInt))
+    this.forceUpdate()
+  }
+
+  sortSubmittedHigh() {
+    this.props.articles.sort(sortBy('publish_at', true))
+    this.forceUpdate()
   }
 
   render() {
@@ -33,13 +58,17 @@ class ArticlesIndex extends React.Component {
     return (
       <div>
         <div className="table-responsive">
-          <table className="table table-striped table-bordered">
+          <table className="table table-striped table-bordered sortable">
             <thead>
               <tr className="table-header vert-align-mid-head">
                 <th><br/>UNPUBLISHED ARTICLES ({this.props.articles.length})<br/><br/></th>
                 <th>AUTHOR<br/><br/></th>
-                <th>WORDS<br/><br/></th>
-                <th>SUBMITTED<br/><br/></th>
+                <th>WORDS
+                  <span onClick={this.sortWordsLow}> <Icon name="caret-up" /></span>
+                  <span onClick={this.sortWordsHigh}> <Icon name="caret-down" /></span><br/><br/></th>
+                <th>SUBMITTED
+                  <span onClick={this.sortSubmittedLow}> <Icon name="caret-up" /></span>
+                  <span onClick={this.sortSubmittedHigh}> <Icon name="caret-down" /></span><br/><br/></th>
               </tr>
             </thead>
             <tbody>
@@ -69,6 +98,16 @@ class ArticlesIndex extends React.Component {
     )
   }
 };
+
+var sortBy = function(field, reverse, primer){
+  var key = primer ?
+  function(x) {return primer(x[field])} :
+  function(x) {return x[field]};
+  reverse = !reverse ? 1 : -1;
+  return function (a, b) {
+    return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+  }
+}
 
 function mapStateToProps(state){
   return {
